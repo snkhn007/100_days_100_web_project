ccfc30f2a49f003a97dd4b54fd9c18af220af2b3
kavita bande <kavitabande2006@gmail.com>
2026-05-22 17:30:16 +0530

    fix: improve form accessibility and resolve login input mismatch

--- public/loginusingmern/index.js
+++ public/loginusingmern/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const bodyParser = require('body-parser');
 const path = require('path');
 const ejs = require('ejs');
 const collection = require('./mongo.js');
@@ -10,6 +9,9 @@ const validator = require('validator')
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.get('/', (req, res) => { res.render('login'); });
+app.get('/login', (req, res) => {
+  res.render('login');
+});
 app.get('/signup', (req, res) => { res.render('signup'); });
 app.get('/home', (req, res) => { res.render('home'); });
 app.use(express.static(__dirname + '/public'));
